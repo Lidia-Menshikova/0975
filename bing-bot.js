3eeb91acd7b4348a5eff01fccd1877c8e3ffe6a8
Lidia-Menshikova <116346800+Lidia-Menshikova@users.noreply.github.com>
2022-11-09 16:18:06 +0300

    Переходит с главной на страницу выдачи
    1. Работает на главной странице поисковика.
    2. Определяет, где находится (главная или выдача).
    3. Рандомно выбирает ключевые фразы.
    4. Переходит к результатам выдачи.
    5. Выбирает релевантный результат и переходит по выбранной ссылке.

---
+++ bing-bot.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Bing Bot
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Bot for Bing
+// @author       Lidia Menshikova
+// @match        https://www.bing.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+let links = document.links;
+let btnSearch = document.getElementById("search_icon");
+let keywords = [
+  "10 самых популярных шрифтов от Google",
+  "Отключение редакций и ревизий в WordPress",
+  "Вывод произвольных типов записей и полей в WordPress",
+];
+let keyword = keywords[getRandom(0, keywords.length)];
+
+if (window.location.toString().includes("search")) {
+  for (let i = 0; i < links.length; i++) {
+    if (links[i].href.indexOf("napli.ru") !== -1) {
+      let link = links[i];
+      link.click();
+      alert("Нашел строку: " + links[i]);
+      break;
+    }
+  }
+} else {
+  document.getElementsByName("q")[0].value = keyword;
+  btnSearch.click();
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
